5158a52bd9238fc5650228a54c619f2e1e691cf5
Christopher Parsons <cparsons@habaneroconsulting.com>
2015-05-10 18:51:15 -0700

    Added solution to 018

---
+++ problems/01x/018.js
@@ -0,0 +1,23 @@
+/**
+ * problems/01x/018.js
+ */
+
+module.exports = function (triangle) {
+	// Loop through each row of the triangle
+	for (var y = 1; y < triangle.length; y++) {
+		// Loop through each item in the row
+		for (var x = 0; x < triangle[y].length; x++) {
+			// If we're on the left side, take the only element
+			// Otherwise, take the larger parent element
+			if (x === 0 || triangle[y - 1][x - 1] < triangle[y - 1][x]) {
+				triangle[y][x] += triangle[y - 1][x];
+			} else {
+				triangle[y][x] += triangle[y - 1][x - 1];
+			}
+		}
+	}
+
+	return triangle[triangle.length - 1].reduce(function (maximum, sum) {
+		return sum > maximum ? sum : maximum;
+	}, 0);
+};
