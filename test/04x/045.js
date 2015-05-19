8fb2421abdaa90da4be6ed4f9f48c02a244e00f5
Christopher Parsons <cparsons@habaneroconsulting.com>
2015-05-18 19:19:18 -0700

    Added missing test for 045

--- test/04x/045.js
+++ test/04x/045.js
@@ -6,7 +6,7 @@ var problem = require('../../problems/04x/045.js');
 var assert = require('assert');
 
 describe('Problem #045', function () {
-	// it('should return x when the limit is y', function () {
-		// assert.equal(x, problem(y));
-	// });
+	it('should return 1,533,776,805 when the starting point is 285', function () {
+		assert.equal(1533776805, problem(286));
+	});
 });
